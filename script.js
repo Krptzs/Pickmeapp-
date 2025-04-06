825ff5bbbcc88707f4c68f73709f14d398056bbd
Krptzs <pickmeapp.vercel@gmail.com>
2025-04-06 17:16:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("A PickmeApp működik!");
+function openTab(tabName) {
+    // Először elrejtjük az összes tab-t
+    var tabs = document.querySelectorAll('.tab-content');
+    tabs.forEach(function(tab) {
+        tab.classList.remove('active');
+    });
+
+    // Majd megjelenítjük azt a tab-ot, amelyet a felhasználó választott
+    var activeTab = document.getElementById(tabName);
+    activeTab.classList.add('active');
+}
